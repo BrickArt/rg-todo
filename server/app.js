75ce953820b3a17e96ea2bb35991282b7a7e213c
Artem Rebryk <dedok8099@gmail.com>
2018-02-28 09:39:54 +0200

    header

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "https://rebryk-artem.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://todo-for-nothing.herokuapp.com");
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
