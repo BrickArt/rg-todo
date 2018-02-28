75df97dc87725f4fbe75ffc1f19c30fefdaabdc5
Artem Rebryk <artemrebrik8099@gmail.com>
2018-02-28 09:50:31 +0200

    send file

--- server/src/router/index.router.js
+++ server/src/router/index.router.js
@@ -6,7 +6,7 @@ const Taskboard = require("../models/taskboard.model").Taskboard;
 
 router.route('/')
     .get(function(req, res, next) {
-        res.status(200).sendFile("../../../client/dist/index")
+        res.status(200).sendFile("../../../client/dist/index.html")
     })
 
 router.route('/users')
